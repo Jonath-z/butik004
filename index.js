f529c632a42f3d8fe9acf103786e618ed70976e1
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2021-04-17 19:26:59 +0200

    switching upload directory

--- index.js
+++ index.js
@@ -17,141 +17,107 @@ app.use('/public', express.static(path.join('./uploads_Airforce1')));
 
 const AirF = 'Air force 1';
 const AirJ = 'Jordan 1';
+   
+        
+        // set storage
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        switch (req.body.model) {
+            case AirF:
+                cb(null,'./uploads_Airforce1/');
+                break;
+            case AirJ:
+                cb(null,'./uploads_Jordan1/');
+                break;
+        }
+        console.log(req);
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+        //  init upload
+const upload = multer({
+    storage: storage,
+    limits: { filesize: 3000000 },
+    fileFilter: function (req, file, cb) {
+        checkFileType(file, cb);
+    }
+}).single('imageFile');
+
+        // check file type
+
+function checkFileType(file, cb) {
+    const filetype = /jpeg|jpg|png/;
+    const extname = filetype.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = filetype.test(file.mimetype);
+
+    if (mimetype && extname) {
+        return cb(null, true);
+    } else {
+        cb('please images only');
+    }
+
+}
 
 
-
-
-
-
-        // // set storage 
-        // const storage = multer.diskStorage({
-        //     destination: './uploads_Airforce1/',
-        //     filename: function (req, file, cb) {
-        //         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-        //     }
-        // });
-
-        // //  init upload
-        // const upload = multer({
-        //     storage: storage,
-        //     limits: { filesize: 3000000 },
-        //     fileFilter: function (req, file, cb) {
-        //         checkFileType(file, cb);
-        //     }
-        // }).single('imageFile');
-
-        // // check file type
-
-        // function checkFileType(file, cb) {
-        //     const filetype = /jpeg|jpg|png/;
-        //     const extname = filetype.test(path.extname(file.originalname).toLowerCase());
-        //     const mimetype = filetype.test(file.mimetype);
-
-        //     if (mimetype && extname) {
-        //         return cb(null, true);
-        //     } else {
-        //         cb('please images only');
-        //     }
-        // }
-    
-        // app.post('/upload', (req, res) => {
-        //     upload(req, res, (err) => {
-        //         if (err) {
-        //             console.log(err);
-        //             res.render('butik', {
-        //                 msg: (err)
-        //             });
-        //         }
-        //         else {
-        //             console.log(req.body);
-        //             console.log(req.file);
-        //             fs.readdir('./uploads_Airforce1', (err, data) => {
-        //                 if (err) {
-        //                     console.log(err);
-        //                 }
-        //                 else {
-        //                     console.log(data);
-        //                     res.render('index', {
-        //                         imgData: data,
-        //                         type: req.body.model
-        //                     });
-        //                 }
-        //             });
-        //         }
-        //     });
-            
-        // });
-app.get('/butik/seller/upload', (req, res) => {
+app.get('/butik/seller/upload',function (req, res){
     res.render('butik');
 });
 
 
-    switch (req.body.model) {
-
-        case AirJ:
-            // set storage 
-            const storage = multer.diskStorage({
-                destination: './uploads_Jordan1/',
-                filename: function (req, file, cb) {
-                    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-                }
+    
+app.post('/', (req, res) => {
+    upload(req, res, (err) => {
+        if (err) {
+            console.log(err);
+            res.render('butik', {
+                msg: (err)
             });
-
-            //  init upload
-            const upload = multer({
-                storage: storage,
-                limits: { filesize: 3000000 },
-                fileFilter: function (req, file, cb) {
-                    checkFileType(file, cb);
-                }
-            }).single('imageFile');
-
-            // check file type
-
-            function checkFileType(file, cb) {
-                const filetype = /jpeg|jpg|png/;
-                const extname = filetype.test(path.extname(file.originalname).toLowerCase());
-                const mimetype = filetype.test(file.mimetype);
-
-                if (mimetype && extname) {
-                    return cb(null, true);
-                } else {
-                    cb('please images only');
-                }
-            }
-            
-            app.post('/upload', (req, res) => {
-                upload(req, res, (err) => {
-                    if (err) {
-                        console.log(err);
-                        res.render('butik', {
-                            msg: (err)
-                        });
-                    }
-                    else {
-                        console.log(req.body);
-                        console.log(req.file);
-                        fs.readdir('./uploads_Jordan1', (err, data) => {
+        }
+        else {
+            switch (req.body.model) {
+                case AirJ:
+           
+                    fs.readdir('./uploads_Jordan1', (err, data) => {
+                        if (err) {
+                            console.log(err);
+                        }
+                        else {
+                            res.render('index', {
+                                imgData: data,
+                                size: req.range(),
+                                price: req.number,
+                                model: req.text,
+                                Req: req.file
+                            });
+    
+                        }
+                    });
+                    break;
+                    case AirF:
+           
+                        fs.readdir('./uploads_Airforce1', (err, data) => {
                             if (err) {
                                 console.log(err);
                             }
                             else {
-                                console.log(data);
-                                console.log(req.body.model);
-                                res.render('jordan1', {
+                                res.render('index', {
                                     imgData: data,
-                                    type: req.body.model
+                                    size: req.range(),
+                                    price: req.number,
+                                    model: req.text,
+                                    Req: req.file
                                 });
+        
                             }
                         });
-                    }
-                });
-            });
-            break;
-        default:
-            res.render('butik');
-            res.send('error');
-    }
-
-
+                        break;
+                default:
+                    res.render('butik');
+                   
+            }
+        }
+    });
+});
 app.listen(3000);
