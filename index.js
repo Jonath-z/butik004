8968752762cb63dd2e0433799c2207e60381d861
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2021-04-26 14:55:10 +0200

    data base bug

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const GridFsStorage = require('multer-gridfs-storage');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
 const Datastore = require('nedb');
 const eventEmitter = require('events');
+const { url } = require('inspector');
+const { config } = require('process');
+const { resolve } = require('path');
+const { reject } = require('lodash');
 const Events = new eventEmitter();
 const app = express();
 
@@ -15,99 +23,55 @@ app.use('/statics', express.static(path.join(__dirname, '/js_file')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/public', express.static(path.join('./uploads_Jordan1')));
 app.use('/public', express.static(path.join('./uploads_Airforce1')));
-   
 
-// set database
-const database2 = new Datastore('descript.db');
-database2.loadDatabase();
+const URL = "mongodb+srv://jo_z:25112002@jonathan@cluster0.h1rni.mongodb.net/jo_z?retryWrites=true&w=majority";
+
+// setup storage engine
+const storage = new GridFsStorage({
+    url: URL,
+    file: function (req, file) {
+        return new Promise((resolve, reject) => {
+            crypto.randomBytes(16, (err, buf) => {
+                if (err) {
+                    return reject(err);
+                }
+                else {
+                    const filename = buf.toString('hex') + path.extname(file.originalname);
+                    const fileInfo = {
+                        filename: filename,
+                        buketname: 'uploads'
+                    }
+                    resolve(fileInfo);
+                }
+            });
+        });
+    }
+});
+
+const upload = multer({ storage });
+
+const connect = mongoose.createConnection(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+let gfs;
+connect.once('open', () => {
+    gfs = new mongoose.mongo.GridFSBucket(connect.db, {
+        bucketName: "uploads"
+    });
+});
 
 const AirF = 'Air force 1';
 const AirJ = 'Jordan 1';
 
-        // set storage
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './uploads_Airforce1/');
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    }
-});
-                //  init upload
-const upload = multer({
-    storage: storage,
-    limits: { filesize: 3000000 },
-    fileFilter: function (req, file, cb) {
-        checkFileType(file, cb);
-    }
-}).single('Image');
-
-    // check file type
-
-function checkFileType(file, cb) {
-    const filetype = /jpeg|jpg|png/;
-    const extname = filetype.test(path.extname(file.originalname).toLowerCase());
-    const mimetype = filetype.test(file.mimetype);
-
-    if (mimetype && extname) {
-        return cb(null, true);
-    } else {
-        cb('please images only');
-    }
-
-}
-
-app.get('/butik/seller/upload',function (req, res){
+app.get('/uploads/butik', (req, res) => {
     res.render('butik');
-    
+    console.log(req.body);
 });
 
-
-
-app.post('/uploads', (req, res) => {
-
-    database2.insert({
-        size: req.body.size,
-        model: req.body.model,
-        price: req.body.price
-    });
-
-    upload(req, res, (err) => {
-        if (err) {
-            console.log(err);
-            res.render('butik', {
-                msg: (err)
-            });
-        }
-        else {
-
-            if (err)
-                console.log(err);
-            else{
-                fs.readdir('./uploads_Airforce1', (err, data) => {
-                    
-                    if (err) {
-                        console.log(err);
-                    }
-                    else {
-                        for (let i = 0; i < data.length;i++) {
-                            database2.insert({
-                                imgLink: i
-                            });
-                        }
-                        if (err) {
-                            console.log(err);
-                        }
-                        else {
-                            res.render('index', {
-                                imgData: data,
-                            });
-                        }
-                    }
-                });
-            }
-        }
-    });
+app.post('/', upload.single("Image"), (req, res) => {
+    res.render('index');
 });
 
  
