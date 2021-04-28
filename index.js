34e4720fbb1d92f92c480fe0430941b8a385c3b3
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2021-04-28 13:49:10 +0200

    bug on uploading file with schema on mongo db

--- index.js
+++ index.js
@@ -14,77 +14,81 @@ const { url } = require('inspector');
 const { config } = require('process');
 const { resolve } = require('path');
 const { reject } = require('lodash');
+const { stringify } = require('querystring');
 const Events = new eventEmitter();
 const app = express();
 
-app.set('view engine', 'ejs');
 
+// middleware
+app.set('view engine', 'ejs');
 app.use('/statics', express.static(path.join(__dirname, '/js_file')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/public', express.static(path.join('./uploads_Jordan1')));
 app.use('/public', express.static(path.join('./uploads_Airforce1')));
 
-const uri = "mongodb+srv://jo_z:2511@cluster0.h1rni.mongodb.net/admin?retryWrites=true&w=majority";
+const uri = "mongodb+srv://joz:2511@butik.qrb2j.mongodb.net/butik?retryWrites=true&w=majority";
+// database connection
+mongoose.createConnection(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // setup storage engine
-const storage = new GridFsStorage({
-    url: uri,
-    file: function (req, file) {
-        return new Promise((resolve, reject) => {
-            crypto.randomBytes(16, (err, buf) => {
-                if (err) {
-                    return reject(err);
-                }
-                else {
-                    const filename = buf.toString('hex') + path.extname(file.originalname);
-                    const fileInfo = {
-                        filename: filename,
-                        buketname: 'uploads'
-                    }
-                    resolve(fileInfo);
-                }
-            });
-        });
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now()+file.originalname);
+        console.log(file);
     }
 });
 
 const upload = multer({ storage }).single('Image');
 
-const connect = mongoose.createConnection(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-// const MongoClient = require('mongodb').MongoClient;
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// client.connect(err => {
-//     const collection = client.db("test").collection("devices");
-//     // perform actions on the collection object
-//     client.close();
-//   });
-let gfs;
-connect.once('open', () => {
-    gfs = new mongoose.mongo.GridFSBucket(connect.db, {
-        bucketName: "uploads"
-    });
+// docs schema
+const schema = new mongoose.Schema({
+    model: String,
+    size: Number,
+    price: Number,
+    image: {
+        data: Buffer,
+        contentType: String
+    }
 });
 
-const AirF = 'Air force 1';
-const AirJ = 'Jordan 1';
+// creating model
+const Detail = mongoose.model('Detail', schema);
 
+
+// get request
 app.get('/uploads/butik', (req, res) => {
     res.render('butik');
-    console.log(req.body);
 });
+app.get('/butik', (req, res) => {
+    res.render('index')
+})
 
-app.post('/', (req, res) => {
-    upload(req, res, (err) => {
+// post request
+app.post('/uploads/butik', upload, (req, res) => {
+
+    const obj = {
+        model: req.body.model,
+        size: req.body.size,
+        price: req.body.size,
+        image: {
+            data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+        }
+    };
+    console.log(obj);
+    Detail.create(obj, (err, item) => {
         if (err) {
             console.log(err);
         }
         else {
+            item.save();
             res.render('index');
         }
-    });
+    })
+    // console tst
+    console.log(req.body);
 });
-
- 
 
 app.listen(3000);
